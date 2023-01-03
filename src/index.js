b3964dfd29a6190c883b29ee9001a9e160bcdfbe
thewoodfish <jasonholt2002@gmail.com>
2023-01-03 11:54:09 +0100

    setting up necessary config

---
+++ src/index.js
@@ -0,0 +1,27 @@
+// imports
+import * as Kilt from '@kiltprotocol/sdk-js'
+import axios from 'axios'
+
+class SamaritanSDK {
+    connected = false;
+    kilt_connector = {};
+
+    // initialize the library instance
+    init() {
+        if (!connected) {
+            let kilt_con = [];
+
+            sam.db.insert(did, {});
+        }
+    }
+
+    // database connections
+    db = {
+        insert() {
+
+        }
+    }
+
+}
+
+module.exports = SamaritanSDK;
